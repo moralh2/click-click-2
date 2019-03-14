427e19e82cfead3173c5126e008e20ffbf401a27
Hector M <moralh2@gmail.com>
2019-03-14 00:17:28 -0500

    testing using cards

--- src/App.js
+++ src/App.js
@@ -6,12 +6,41 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <div class="alert alert-primary" role="alert">
-          A simple primary alert—check it out!
+
+        <div className="card col-sm-4">
+          <img src={imageSrc} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+            <p className="card-text">Dexterino</p>
+          </div>
         </div>
+
+        <div className="card col-sm-4">
+          <img src={imageSrc} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+            <p className="card-text">Dexterino</p>
+          </div>
+        </div>
+
+        <div className="card col-sm-4">
+          <img src={imageSrc} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+            <p className="card-text">Dexterino</p>
+          </div>
+        </div>
+
+        <div className="card col-sm-4">
+          <img src={imageSrc} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+            <p className="card-text">Dexterino</p>
+          </div>
+        </div>
+
       </Wrapper>
     );
   }
 }
+
+
+var imageSrc = "https://s-media-cache-ak0.pinimg.com/originals/fe/32/49/fe32495d45283cd6860ae122f0aeaad9.png"
 
 export default App;
