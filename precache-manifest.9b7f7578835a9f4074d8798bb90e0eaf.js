430a78d28204f1610b658223655c15dd01999ec3
Hector M <moralh2@gmail.com>
2019-03-13 23:15:09 -0500

    Updates

---
+++ precache-manifest.9b7f7578835a9f4074d8798bb90e0eaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/click-click-2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a979eac5cbb44c3b5d8e",
+    "url": "/click-click-2/static/js/runtime~main.f5c485f0.js"
+  },
+  {
+    "revision": "dcc07e9e1dd8ddf8ec4e",
+    "url": "/click-click-2/static/js/main.80f96fdb.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/click-click-2/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "dcc07e9e1dd8ddf8ec4e",
+    "url": "/click-click-2/static/css/main.8696ba2f.chunk.css"
+  },
+  {
+    "revision": "8a56c24332dbd47ca6a43cb4c7806438",
+    "url": "/click-click-2/index.html"
+  }
+];
