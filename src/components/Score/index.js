46de7c8c1745b0c796d9953579c2c637635c6a8a
Hector M <moralh2@gmail.com>
2019-03-15 19:02:34 -0500

    more css changes

--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -3,11 +3,11 @@ import "./style.css"
 
 function Score(props) {
     return (
-        <div className="score-wrapper text-center">
-            <button type="button" className="mr-4 btn btn-warning">
+        <div className="score-wrapper btn-group text-center" role="group">
+            <button type="button" className={"btn " + (props.score === 12 ? 'btn-success' : 'btn-score')}>
                 Score <span className="badge badge-light">{props.score}</span>
             </button>
-            <button type="button" className={"ml-4 btn " + (props.score === 12 ? 'btn-success' : 'btn-primary')}>
+            <button type="button" className={"btn " + (props.topScore === 12 ? 'btn-success' : 'btn-topScore')}>
                 Top Score <span className="badge badge-light">{props.topScore}</span>
             </button>
         </div>
